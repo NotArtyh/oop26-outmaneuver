588fcaa3500ef956cd8b810496280bf89d74415b
Spinaci Gianmarco <gianmarco.spinaci3@studio.unibo.it>
2026-06-16 09:30:23 +0200

    feat: abstraction of class entity and refactoring of game area element

--- src/main/java/outmaneuver/model/collectibles/StarCollectible.java
+++ src/main/java/outmaneuver/model/collectibles/StarCollectible.java
@@ -2,12 +2,14 @@ package outmaneuver.model.collectibles;
 
 import outmaneuver.model.area.Plane;
 import outmaneuver.model.session.IGameSession;
+import outmaneuver.util.Vector2;
 
-public final class StarCollectible implements Collectible {
+public final class StarCollectible extends AbstractCollectible {
 
     private final int scoreValue;
 
-    public StarCollectible(final int scoreValue) {
+    public StarCollectible(final Vector2 position, final int scoreValue) {
+        super(position); // Initialize position with the provided value
         if (scoreValue <= 0) {
             throw new IllegalArgumentException("scoreValue must be positive");
         }
--- src/main/java/outmaneuver/model/collision/CollisionLayer.java
+++ src/main/java/outmaneuver/model/collision/CollisionLayer.java
@@ -2,5 +2,6 @@ package outmaneuver.model.collision;
 
 public enum CollisionLayer {
     PLANE,
-    MISSILE
+    MISSILE,
+    COLLECTIBLE
 }
