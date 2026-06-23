0019f5593716d7afdbd545c85e2a52c8f7e6e1d4
Arthur Muller <arty97muller@gmail.com>
2026-06-24 01:27:24 +0200

    feat: refacto GameEventController suppport entityControllers

--- src/main/java/outmaneuver/controller/impl/GameEventControllerImpl.java
+++ src/main/java/outmaneuver/controller/impl/GameEventControllerImpl.java
@@ -2,37 +2,36 @@ package outmaneuver.controller.impl;
 
 import java.util.Objects;
 
-import outmaneuver.controller.EntityController;
 import outmaneuver.controller.GameEventController;
-import outmaneuver.controller.HudController;
 import outmaneuver.controller.ScoreController;
 import outmaneuver.controller.event.CollisionEvent;
+import outmaneuver.controller.event.EffectEvent;
 import outmaneuver.controller.event.Event;
 
+import outmaneuver.controller.impl.missile.MissileControllerImpl;
 import outmaneuver.model.area.collision.CollisionData;
 import outmaneuver.model.area.entity.Entity;
 import outmaneuver.model.area.entity.collectibles.Collectible;
-import outmaneuver.model.area.entity.plane.Plane;
-import outmaneuver.model.session.IGameSession;
+import outmaneuver.model.area.entity.collectibles.ShieldPowerUp;
+import outmaneuver.model.area.entity.collectibles.SpeedBoost;
 
 public final class GameEventControllerImpl implements GameEventController {
 
-    private final EntityController primaryEntityController;
-    private final HudController hudController;
+    private final PlaneControllerImpl planeController;
+    private final CollectibleControllerImpl collectibleController;
+    private final MissileControllerImpl missileController;
     private final ScoreController scoreController;
-    private final IGameSession session;
     private final Runnable onGameOver;
+    private boolean shieldActive;
 
     public GameEventControllerImpl(
-            final EntityController primaryEntityController,
-            final HudController hudController,
+            final MasterControllerImpl master,
             final ScoreController scoreController,
-            final IGameSession session,
             final Runnable onGameOver) {
-        this.primaryEntityController = Objects.requireNonNull(primaryEntityController, "primaryEntityController must not be null");
-        this.hudController = Objects.requireNonNull(hudController, "hudController must not be null");
+        this.planeController = master.getEntityController(PlaneControllerImpl.class).orElseThrow();
+        this.collectibleController = master.getEntityController(CollectibleControllerImpl.class).orElseThrow();
+        this.missileController = master.getEntityController(MissileControllerImpl.class).orElseThrow();
         this.scoreController = scoreController;
-        this.session = Objects.requireNonNull(session, "session must not be null");
         this.onGameOver = Objects.requireNonNull(onGameOver, "onGameOver must not be null");
     }
 
@@ -44,25 +43,24 @@ public final class GameEventControllerImpl implements GameEventController {
 
         switch ((CollisionEvent) evt) {
             case PLANE_MISSILE_COLLISION -> {
-                primaryEntityController.removeEntity((Entity) collisionData.getEntityA());
-                final Plane plane = (Plane) collisionData.getEntityB();
-                if (!plane.isShieldActive()) {
+                planeController.removeEntity((Entity) collisionData.getEntityA());
+                if (!shieldActive) {
                     onGameOver.run();
                 }
             }
             case PLANE_COLLECTIBLE_COLLISION -> {
-                final Plane plane = (Plane) collisionData.getEntityA();
-                final Collectible collectible = (Collectible) collisionData.getEntityB();
-                collectible.apply(plane, session);
-                primaryEntityController.removeEntity(collectible);
-                hudController.onInternalEvent(CollisionEvent.PLANE_COLLECTIBLE_COLLISION, collectible);
+                final var collectible = (Collectible) collisionData.getEntityB();
+                if (collectible.getEffect() != null) {
+                    collectibleController.addEffect(collectible.getEffect());
+                }
+                collectibleController.removeEntity(collectible);
                 if (scoreController != null) {
                     scoreController.onInternalEvent(CollisionEvent.PLANE_COLLECTIBLE_COLLISION, collectible);
                 }
             }
             case MISSILE_MISSILE_COLLISION -> {
-                primaryEntityController.removeEntity((Entity) collisionData.getEntityA());
-                primaryEntityController.removeEntity((Entity) collisionData.getEntityB());
+                missileController.removeEntity((Entity) collisionData.getEntityA());
+                missileController.removeEntity((Entity) collisionData.getEntityB());
                 if (scoreController != null) {
                     scoreController.onInternalEvent(CollisionEvent.MISSILE_MISSILE_COLLISION, collisionData);
                 }
